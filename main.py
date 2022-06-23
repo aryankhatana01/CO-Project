0a312eb07538d6503bfed8016a6cfba2c15bc06d
Audrik21242 <audrik21242@iiitd.ac.in>
2022-06-23 14:15:42 +0530

    test

--- main.py
+++
@@ -1,9 +0,0 @@
-import math
-import sys
-
-commands = []
-with open("Input.txt", 'r') as f:
-    commands_ = f.readlines()
-    for command in commands_:
-        commands.append(command[:-1])
-print(commands)
