3bb8275e7413c7149e820c801cd70f66b645ef9b
Audrik21242 <audrik21242@iiitd.ac.in>
2022-06-23 14:18:43 +0530

    added the space

---
+++ main.py
@@ -0,0 +1,10 @@
+import math
+import sys
+
+commands = []
+with open("Input.txt", 'r') as f:
+    commands_ = f.readlines()
+    for command in commands_:
+        commands.append(command[:-1])
+
+print(commands)
