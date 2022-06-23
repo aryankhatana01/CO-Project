1f26506941a90d63e470a069d23d2f9772430e3d
Aryan Khatana <51334629+aryankhatana01@users.noreply.github.com>
2022-06-23 18:03:51 +0530

    Move Operation Implemented

--- main.py
+++ main.py
@@ -7,15 +7,31 @@ def decimalToBinary(n):
 
 reg = ["R0", "R1", "R2", "R3", "R4", "R5", "R6"]
 
-commands = []
+# commands = []
 with open("Input.txt", 'r') as f:
-    commands_ = f.readlines()
-    for command in commands_:
-        commands.append(command[:-1])
+    commands = f.read().splitlines() 
 # print(opcode)
 # print(commands)
 ans = []
+# print(commands)
 for command in commands:
     instruction = command.split(' ')
-    # if instruction[0]=="mov":
-    #     if instruction[2] in reg:
+    if instruction[0]=="mov":
+        if instruction[2] in reg:
+            a = opcode["mov2"]
+            r = registers[instruction[1]] + registers[instruction[2]]
+            zeroes = 11-len(r)
+            ans.append(a+zeroes+r)
+        else:
+            a = opcode["mov1"]
+            r = registers[instruction[1]]
+            num = instruction[2][1:]
+            # print(num)
+            im = decimalToBinary(int(num))
+            im = str(im)
+            zeroes = 8-len(im)
+            zeroes = "0"*zeroes
+            # print(type(im))
+            # print(z)
+            ans.append(a+r+zeroes+im)
+print(ans)
