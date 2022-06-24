fa99ad843fd5023b9a027a6f5f02c5ca69e9124e
AyushMelty <ayushmelty@gmail.com>
2022-06-25 00:27:50 +0530

    error handling of registers

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ def decimalToBinary(n):
     return bin(n).replace("0b", "")
 
 reg = ["R0", "R1", "R2", "R3", "R4", "R5", "R6"]
+error=0
 
 # commands = []
 with open("Input.txt", 'r') as f:
@@ -22,12 +23,20 @@ else:
         instruction = command.split(' ')
         if instruction[0]=="mov":
             if instruction[2] in reg:
+                if (instruction[1] not in reg) or (instruction[2] not in reg):
+                    print("Error: register not valid")
+                    error=1
+                    break
                 a = opcode["mov2"]
                 r = registers[instruction[1]] + registers[instruction[2]]
                 zeroes = 11-len(r)
                 zeroes = "0"*zeroes
                 ans.append(a+zeroes+r)
             else:
+                if (instruction[1] not in reg):
+                    print("Error: register not valid")
+                    error=1
+                    break
                 a = opcode["mov1"]
                 r = registers[instruction[1]]
                 num = instruction[2][1:]
@@ -37,6 +46,7 @@ else:
                 zeroes = 8-len(im)
                 if zeroes < 0:
                     print("Error: number is too large")
+                    error=1
                     break
                 else:
                     zeroes = "0"*zeroes
@@ -45,6 +55,10 @@ else:
                     ans.append(a+r+zeroes+im)
 
         elif instruction[0]=="add":
+            if (instruction[1] not in reg) or (instruction[2] not in reg) or (instruction[3] not in reg):
+                print("Error: register not valid")
+                error=1
+                break
             a = opcode["add"]
             r = registers[instruction[1]] + registers[instruction[2]] + registers[instruction[3]]
             zeroes = 11-len(r)
@@ -52,6 +66,10 @@ else:
             ans.append(a+zeroes+r)
         
         elif instruction[0]=="mul":
+            if (instruction[1] not in reg) or (instruction[2] not in reg) or (instruction[3] not in reg):
+                print("Error: register not valid")
+                error=1
+                break
             a = opcode["mul"]
             r = registers[instruction[1]] + registers[instruction[2]] + registers[instruction[3]]
             zeroes = 11-len(r)
@@ -65,6 +83,10 @@ else:
             ans.append(a+zeroes)
         
         elif instruction[0]=="sub":
+            if (instruction[1] not in reg) or (instruction[2] not in reg) or (instruction[3] not in reg):
+                print("Error: register not valid")
+                error=1
+                break
             a = opcode["sub"]
             r = registers[instruction[1]] + registers[instruction[2]] + registers[instruction[3]]
             zeroes = 11-len(r)
@@ -72,6 +94,10 @@ else:
             ans.append(a+zeroes+r)
         
         elif instruction[0]=="div":
+            if (instruction[1] not in reg) or (instruction[2] not in reg):
+                print("Error: register not valid")
+                error=1
+                break
             a = opcode["div"]
             r = registers[instruction[1]] + registers[instruction[2]]
             zeroes = 11-len(r)
@@ -79,6 +105,10 @@ else:
             ans.append(a+zeroes+r)
         
         elif instruction[0]=="ls":
+            if (instruction[1] not in reg):
+                print("Error: register not valid")
+                error=1
+                break
             a = opcode["ls"]
             r = registers[instruction[1]]
             num = instruction[2][1:]
@@ -88,6 +118,7 @@ else:
             zeroes = 8-len(im)
             if zeroes < 0:
                 print("Error: number is too large")
+                error=1
                 break
             else:
                 zeroes = "0"*zeroes
@@ -96,6 +127,10 @@ else:
                 ans.append(a+r+zeroes+im)
         
         elif instruction[0]=="rs":
+            if (instruction[1] not in reg):
+                print("Error: register not valid")
+                error=1
+                break
             a = opcode["rs"]
             r = registers[instruction[1]]
             num = instruction[2][1:]
@@ -105,6 +140,7 @@ else:
             zeroes = 8-len(im)
             if zeroes < 0:
                 print("Error: number is too large")
+                error=1
                 break
             else:
                 zeroes = "0"*zeroes
@@ -113,6 +149,10 @@ else:
                 ans.append(a+r+zeroes+im)
         
         elif instruction[0]=="xor":
+            if (instruction[1] not in reg) or (instruction[2] not in reg) or (instruction[3] not in reg):
+                print("Error: register not valid")
+                error=1
+                break
             a = opcode["xor"]
             r = registers[instruction[1]] + registers[instruction[2]] + registers[instruction[3]]
             zeroes = 11-len(r)
@@ -120,31 +160,50 @@ else:
             ans.append(a+zeroes+r)
         
         elif instruction[0]=="st":
+            if (instruction[1] not in reg):
+                print("Error: register not valid")
+                error=1
+                break
             a = opcode["st"]
             r = registers[instruction[1]]
             d = randomaddress()
             x = d[instruction[2]]
             ans.append(a+r+x)
         elif instruction[0]=="or":
+            if (instruction[1] not in reg) or (instruction[2] not in reg) or (instruction[3] not in reg):
+                print("Error: register not valid")
             a=opcode["or"]
             r=registers[instruction[1]] + registers[instruction[2]] + registers[instruction[3]]
             zeroes="0"*2
             ans.append(a+zeroes+r)
         elif instruction[0]=="and":
+            if (instruction[1] not in reg) or (instruction[2] not in reg) or (instruction[3] not in reg):
+                print("Error: register not valid")
+                error=1
+                break
             a=opcode["and"]
             r=registers[instruction[1]] + registers[instruction[2]] + registers[instruction[3]]
             zeroes="0"*2
             ans.append(a+zeroes+r)
         elif instruction[0]=="not":
+            if (instruction[1] not in reg) or (instruction[2] not in reg):
+                print("Error: register not valid")
+                error=1
+                break
             a=opcode["not"]
             r=registers[instruction[1]] + registers[instruction[2]]
             zeroes="0"*5
             ans.append(a+zeroes+r)
         elif instruction[0]=="cmp":
+            if (instruction[1] not in reg) or (instruction[2] not in reg):
+                print("Error: register not valid")
+                error=1
+                break
             a=opcode["cmp"]
             r=registers[instruction[1]] + registers[instruction[2]]
             zeroes="0"*5
             ans.append(a+zeroes+r)            
             
-    print(ans)
+    if error!=1:
+        print(ans)
 
