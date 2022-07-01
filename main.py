6a191bd811c6bbf5636f4034eecfd263e6147ebd
Audrik21242 <audrik21242@iiitd.ac.in>
2022-07-01 17:36:23 +0530

    *functions*

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from msvcrt import open_osfhandle
 import sys
 from opcodes_dict import opcode, registers
 from randomAddress import randomaddress
+from register_states import reg_states
 
 def decimalToBinary(n):
     return bin(n).replace("0b", "")
@@ -12,11 +13,39 @@ error=0
 
 # commands = []
 with open("Input.txt", 'r') as f:
-    commands = f.read().splitlines() 
+    commands = f.read().splitlines()
 # print(opcode)
 # print(commands)
 ans = []
 # print(commands)
+
+var_addresses = {}
+
+# def add(reg_):
+#     if (instruction[1] not in reg) or (instruction[2] not in reg) or (instruction[3] not in reg):
+#         print("Error: register not valid")
+#         error=1
+#         return
+#     else:
+#         a = opcode["add"]
+#         print(registers)
+#         r = registers[reg_[0]] + registers[reg_[1]] + registers[reg_[2]]
+#         zeroes = 11-len(r)
+#         zeroes = "0"*zeroes
+#         k = a+zeroes+r
+#         ans.append(k)
+#         s = reg_states[reg_[0]] + reg_states[reg_[1]]
+#         reg_states[reg_[2]] = s
+#         return k, s
+
+total_var_ins = 0
+for c in commands:
+    instruction = c.split(' ')
+    if instruction[0] == "var":
+        total_var_ins+=1
+
+what_to_add = 0
+
 if commands[-1] != "hlt":
     print("hlt not being used as the last instruction")
 else:
@@ -30,6 +59,7 @@ else:
                     break
                 a = opcode["mov2"]
                 r = registers[instruction[1]] + registers[instruction[2]]
+                reg_states[registers[instruction[2]]] = reg_states[instruction[1]]
                 zeroes = 11-len(r)
                 zeroes = "0"*zeroes
                 ans.append(a+zeroes+r)
@@ -41,6 +71,7 @@ else:
                 a = opcode["mov1"]
                 r = registers[instruction[1]]
                 num = instruction[2][1:]
+                reg_states[instruction[1]] = num
                 # print(num)
                 im = decimalToBinary(int(num))
                 im = str(im)
@@ -167,9 +198,10 @@ else:
                 break
             a = opcode["st"]
             r = registers[instruction[1]]
-            d = randomaddress()
-            x = d[instruction[2]]
+            # d = randomaddress()
+            x = var_addresses[instruction[2]]
             ans.append(a+r+x)
+            
         elif instruction[0]=="or":
             if (instruction[1] not in reg) or (instruction[2] not in reg) or (instruction[3] not in reg):
                 print("Error: register not valid")
@@ -179,6 +211,7 @@ else:
             r=registers[instruction[1]] + registers[instruction[2]] + registers[instruction[3]]
             zeroes="0"*2
             ans.append(a+zeroes+r)
+
         elif instruction[0]=="and":
             if (instruction[1] not in reg) or (instruction[2] not in reg) or (instruction[3] not in reg):
                 print("Error: register not valid")
@@ -188,6 +221,7 @@ else:
             r=registers[instruction[1]] + registers[instruction[2]] + registers[instruction[3]]
             zeroes="0"*2
             ans.append(a+zeroes+r)
+
         elif instruction[0]=="not":
             if (instruction[1] not in reg) or (instruction[2] not in reg):
                 print("Error: register not valid")
@@ -197,6 +231,7 @@ else:
             r=registers[instruction[1]] + registers[instruction[2]]
             zeroes="0"*5
             ans.append(a+zeroes+r)
+
         elif instruction[0]=="cmp":
             if (instruction[1] not in reg) or (instruction[2] not in reg):
                 print("Error: register not valid")
@@ -244,6 +279,15 @@ else:
         #     z=zeroes
         #     print(a+z+d1)
 
+            ans.append(a+zeroes+r) 
+
+        elif instruction[0]=="var":
+            instruction = instruction[1:]
+            for var in instruction:
+                binary_address = randomaddress(total_var_ins, len(commands), what_to_add)
+                var_addresses[var] = binary_address
+                what_to_add += 1
+            
     if error!=1:
         print(ans)
 
